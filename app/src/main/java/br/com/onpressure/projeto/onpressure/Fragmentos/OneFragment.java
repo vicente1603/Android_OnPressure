51a46c3eff35e2293f5e425f4ce185e22c7eb039
vicente1603 <vicentesantiago1603@gmail.com>
2018-12-03 16:28:46 -0300

    Criação do Tag em Alimentos Activity.

---
+++ app/src/main/java/br/com/onpressure/projeto/onpressure/Fragmentos/OneFragment.java
@@ -0,0 +1,34 @@
+package br.com.onpressure.projeto.onpressure.Fragmentos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import br.com.onpressure.projeto.onpressure.R;
+
+public class OneFragment extends Fragment {
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        ((TextView) view.findViewById(R.id.fragmentText)).setText("One");
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/br/com/onpressure/projeto/onpressure/Fragmentos/SecondFragment.java
@@ -0,0 +1,34 @@
+package br.com.onpressure.projeto.onpressure.Fragmentos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import br.com.onpressure.projeto.onpressure.R;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        ((TextView) view.findViewById(R.id.fragmentText)).setText("Second");
+
+        return view;
+    }
+
+}

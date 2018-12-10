17846c92f488a0c37f797697b946c1cc79120b54
vicente1603 <vicentesantiago1603@gmail.com>
2018-12-10 09:14:24 -0300

    Criação do Tag em Alimentos Activity.  Dti

--- app/src/main/java/br/com/onpressure/projeto/onpressure/Activities/AlimentosActivity.java
+++ app/src/main/java/br/com/onpressure/projeto/onpressure/Activities/AlimentosActivity.java
@@ -40,8 +40,8 @@ public class AlimentosActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "ONE");
-        adapter.addFragment(new SecondFragment(), "TWO");
+        adapter.addFragment(new OneFragment(), "Consumir");
+        adapter.addFragment(new SecondFragment(), "Evitar");
         viewPager.setAdapter(adapter);
     }
 
--- app/src/main/java/br/com/onpressure/projeto/onpressure/Fragmentos/OneFragment.java
+++ app/src/main/java/br/com/onpressure/projeto/onpressure/Fragmentos/OneFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import br.com.onpressure.projeto.onpressure.R;

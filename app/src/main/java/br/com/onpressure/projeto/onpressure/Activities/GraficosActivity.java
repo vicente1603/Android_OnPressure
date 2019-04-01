debdd45b311a8c7f99d1c1cafa5a289f04ec16dd
vicente1603 <vicentesantiago1603@gmail.com>
2019-04-01 15:49:28 -0300

    Inserção das datas dos gráficos

--- app/src/main/java/br/com/onpressure/projeto/onpressure/Activities/GraficosActivity.java
+++ app/src/main/java/br/com/onpressure/projeto/onpressure/Activities/GraficosActivity.java
@@ -9,11 +9,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -33,6 +35,11 @@ public class GraficosActivity extends AppCompatActivity {
     ArrayList<String> dates;
     Random random;
     ArrayList<BarEntry> barEntries;
+    EditText txtDataFim;
+    EditText txtDataInicio;
+    Button btn_pesquisar;
+    Button btn_limpar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,14 +48,40 @@ public class GraficosActivity extends AppCompatActivity {
 
 
         barChart = (BarChart) findViewById(R.id.bargraph);
+        txtDataInicio = findViewById(R.id.txtDataInicio);
+        txtDataFim = findViewById(R.id.txtDataFim);
+        btn_pesquisar = findViewById(R.id.btn_pesquisar);
+        btn_limpar = findViewById(R.id.btn_limpar);
 
-        createRandomBarGraph("2016/05/05", "2016/06/01");
+
+        btn_pesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String dataInicio = txtDataInicio.getText().toString();
+                String dataFim = txtDataFim.getText().toString();
+
+                createRandomBarGraph(dataInicio, dataFim);
+                barChart.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtDataFim.setText("");
+                txtDataInicio.setText("");
+                barChart.setVisibility(View.GONE);
+
+            }
+        });
+
 
     }
 
-    public void createRandomBarGraph(String Date1, String Date2){
+    public void createRandomBarGraph(String Date1, String Date2) {
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
             Date date1 = simpleDateFormat.parse(Date1);
@@ -63,43 +96,43 @@ public class GraficosActivity extends AppCompatActivity {
             mDate2.setTime(date2);
 
             dates = new ArrayList<>();
-            dates = getList(mDate1,mDate2);
+            dates = getList(mDate1, mDate2);
 
             barEntries = new ArrayList<>();
-            float max = 0f;
-            float value = 0f;
+            float max;
+            float value;
             random = new Random();
-            for(int j = 0; j< dates.size();j++){
+            for (int j = 0; j < dates.size(); j++) {
                 max = 5f;
                 value = random.nextFloat() * max;
-                barEntries.add(new BarEntry(value,j));
+                barEntries.add(new BarEntry(value, j));
             }
 
-        }catch(ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        BarDataSet barDataSet = new BarDataSet(barEntries,"Datas");
-        BarData barData = new BarData(dates,barDataSet);
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Datas");
+        BarData barData = new BarData(dates, barDataSet);
         barChart.setData(barData);
     }
 
-    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
+    public ArrayList<String> getList(Calendar startDate, Calendar endDate) {
         ArrayList<String> list = new ArrayList<String>();
-        while(startDate.compareTo(endDate)<=0){
+        while (startDate.compareTo(endDate) <= 0) {
             list.add(getDate(startDate));
-            startDate.add(Calendar.DAY_OF_MONTH,1);
+            startDate.add(Calendar.DAY_OF_MONTH, 1);
         }
         return list;
     }
 
-    public String getDate(Calendar cld){
-        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
-                +cld.get(Calendar.DAY_OF_MONTH);
-        try{
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
-            curDate =  new SimpleDateFormat("yyy/MM/dd").format(date);
-        }catch(ParseException e){
+    public String getDate(Calendar cld) {
+        String curDate = cld.get(Calendar.DAY_OF_MONTH) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
+                + cld.get(Calendar.YEAR);
+        try {
+            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(curDate);
+            curDate = new SimpleDateFormat("dd/MM/yyyy").format(date);
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         return curDate;

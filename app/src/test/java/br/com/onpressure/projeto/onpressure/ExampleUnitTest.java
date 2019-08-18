d2b14ea717359b495ff4f06a823e373e8caf7ff0
Vicente Santiago <vicentesantiago1603@gmail.com>
2019-08-18 16:37:46 -0300

    Teste de unidade (teste)

--- app/src/test/java/br/com/onpressure/projeto/onpressure/ExampleUnitTest.java
+++ app/src/test/java/br/com/onpressure/projeto/onpressure/ExampleUnitTest.java
@@ -1,19 +1,31 @@
 package br.com.onpressure.projeto.onpressure;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.net.ContentHandler;
+import java.util.Calendar;
+import java.util.Date;
+
+import br.com.onpressure.projeto.onpressure.Activities.PressaoArterialActivity;
+import br.com.onpressure.projeto.onpressure.Model.PressaoArterial.PressaoArterial;
+import br.com.onpressure.projeto.onpressure.Model.PressaoArterial.PressaoArterialDAO;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+    PressaoArterialDAO pressaoArterialDAO;
 
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void teste() {
+
+        pressaoArterialDAO = mock(PressaoArterialDAO.class);
+        Mockito.when(pressaoArterialDAO.salvar(80, 80,
+                80,"teste", "18/08/2019")).thenReturn(true);
+
     }
 
 }
